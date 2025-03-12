ee280018268085a45c7cc7a2e4957f42bdf4a7bc
Lettner Tobias <t.lettner@teams.deprag.de>
2025-03-12 22:54:07 +0100

    add prints to shell script for a better overview in the log file

--- latex-build.sh
+++ latex-build.sh
@@ -90,18 +90,30 @@ set -eou pipefail
 drawio_files=$(find ~+ -type f -name "*.drawio")
 
 # Convert the drawio files to pdf files if they are newer or the pdf doesn't exist
+echo "##################################################"
+echo "Converting drawio files to pdf files"
+echo "##################################################"
+echo ""
+
 for drawio_file in $drawio_files
 do
   pdf_file="${drawio_file}.pdf"
+  filename=$(basename "$drawio_file" .drawio)
 
   # Check if drawio file is newer than the pdf file or if the pdf does not exist
   if [[ ! -f "$pdf_file" || "$drawio_file" -nt "$pdf_file" ]]; then
-    echo "Converting: $drawio_file -> $pdf_file"
+    echo "Converting $filename.drawio"
     xvfb-run -a drawio -x -f pdf -o "$pdf_file" --crop -t "$drawio_file" --no-sandbox --disable-gpu 2>&1 | grep -Fvf "/usr/share/latex-build/unwanted-logs.txt"
   else
-    echo "Skipping: $drawio_file (PDF is up-to-date)"
+    echo "Skipping: $filename.drawio (PDF is up-to-date)"
   fi
 done
+
+echo ""
+echo "##################################################"
+echo "Building the latex files"
+echo "##################################################"
+echo ""
 
 #Build the latex files using latexmk and pdflatex
 latexmk -pdf -synctex=1 -halt-on-error -interaction=nonstopmode -file-line-error
